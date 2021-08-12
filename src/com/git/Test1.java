b0d11a401af4c41937b26708df75efa7dcc65a35
admin <admin@dell>
2021-08-12 17:16:45 +0530

    my first commit

---
+++ src/com/git/Test1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String name="Shamshad";
+		if(name=="Shamshad") {
+			System.out.println("you are in");
+		}
+		else {System.out.println("check your name");}
+	}
+
+}
